16c93ee0247bb140761342ee3923020b1369ca3d
yuwenyun <yuwenyun.owen.lee@gmail.com>
2018-12-21 11:35:46 +0800

    add offer algorithm solution

--- src/main/java/com/owen/App.java
+++ src/main/java/com/owen/App.java
@@ -1,9 +1,118 @@
 package com.owen;
 
-public class App 
+import com.owen.algorithm.ListNode;
+import com.owen.algorithm.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class App
 {
+    private final static String space = " ";
+    private final static String forwardSlash = "/";
+    private final static String backwardSlash = "\\";
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    public static <T> void printList(ListNode<T> root)
+    {
+        System.out.print(root.getValue() + space);
+        if(root.getNext() == null) {
+            return;
+        }
+        else{
+            printList(root.getNext());
+        }
+    }
+
+    public static <T> void printTree(TreeNode<T> root)
+    {
+        int maxLevel = maxLevel(root);
+        printTree(Collections.singletonList(root), 1, maxLevel);
+    }
+
+    // print the tree line by line
+    private static <T> void printTree(List<TreeNode<T>> nodes, int level, int maxLevel)
+    {
+        if(nodes.isEmpty() || nodes.stream().allMatch(Objects::isNull)){
+            return;
+        }
+
+        int floor = maxLevel - level;
+        int heightOfLinesBetweenLevel = (int)Math.pow(2, floor - 1);
+        int headSpacesCountBeforeChar = (int)Math.pow(2, floor) - 1;
+        int betweenSpacesCountBetweenChilds = (int)Math.pow(2, floor + 1) - 1;
+
+        // print space of the line before print any char
+        printSpace(headSpacesCountBeforeChar);
+
+        // print the char and prepare it's child nodes
+        List<TreeNode<T>> childNodes = new ArrayList<>();
+        for(TreeNode<T> node : nodes){
+            if(node != null){
+                System.out.print(node.getValue());
+                childNodes.add(node.getLeft());
+                childNodes.add(node.getRight());
+            }
+            // add null to the list making it like a full tree
+            else{
+                childNodes.add(null);
+                childNodes.add(null);
+                System.out.print(space);
+            }
+            printSpace(betweenSpacesCountBetweenChilds);
+        }
+        System.out.println("");
+
+        // print forward/backward slashes
+        for(int i = 1; i <= heightOfLinesBetweenLevel; i++){
+            for(int j = 0; j < nodes.size(); j++){
+                printSpace(headSpacesCountBeforeChar - i);
+
+                if(nodes.get(j) == null){
+                    printSpace(2 * heightOfLinesBetweenLevel + i + 1);
+                    continue;
+                }
+
+                if(nodes.get(j).getLeft() != null){
+                    System.out.print(forwardSlash);
+                }
+                else{
+                    printSpace(1);
+                }
+
+                printSpace(2 * i - 1);
+
+                if(nodes.get(j).getRight() != null){
+                    System.out.print(backwardSlash);
+                }
+                else{
+                    printSpace(1);
+                }
+
+                printSpace(2 * heightOfLinesBetweenLevel - i);
+            }
+            System.out.println("");
+        }
+
+        printTree(childNodes, level + 1, maxLevel);
+    }
+
+    private static <T> int maxLevel(TreeNode<T> node)
+    {
+        if(node == null){
+            return 0;
+        }
+        return Math.max(maxLevel(node.getLeft()), maxLevel(node.getRight())) + 1;
+    }
+
+    private static void printSpace(int count)
+    {
+        System.out.print(new String(new char[count]).replace("\0", space));
     }
 }
--- src/main/java/com/owen/ModelFactory.java
+++ src/main/java/com/owen/ModelFactory.java
@@ -1,5 +1,7 @@
 package com.owen;
 
+import com.owen.algorithm.ListNode;
+import com.owen.algorithm.TreeNode;
 import com.owen.common.Employee;
 
 import java.util.ArrayList;
@@ -63,5 +65,71 @@ public class ModelFactory
         emps.add(emp4);
         emps.add(getEmploye());
         return emps;
+    }
+
+    public static ListNode<Integer> getList(){
+        ListNode<Integer> node1 = new ListNode<>(3);
+        ListNode<Integer> node2 = new ListNode<>(1);
+        ListNode<Integer> node3 = new ListNode<>(9);
+        ListNode<Integer> node4 = new ListNode<>(6);
+        ListNode<Integer> node5 = new ListNode<>(8);
+        ListNode<Integer> node6 = new ListNode<>(2);
+        ListNode<Integer> node7 = new ListNode<>(5);
+        ListNode<Integer> node8 = new ListNode<>(4);
+        ListNode<Integer> node9 = new ListNode<>(0);
+        ListNode<Integer> node10 = new ListNode<>(7);
+        node1.setNext(node2);
+        node2.setNext(node3);
+        node3.setNext(node4);
+        node4.setNext(node5);
+        node5.setNext(node6);
+        node6.setNext(node7);
+        node7.setNext(node8);
+        node8.setNext(node9);
+        node9.setNext(node10);
+        return node1;
+    }
+
+    public static TreeNode<Integer> getTree()
+    {
+        TreeNode<Integer> root = new TreeNode<>(3);
+        TreeNode<Integer> node1 = new TreeNode<>(1);
+        TreeNode<Integer> node2 = new TreeNode<>(9);
+        TreeNode<Integer> node3 = new TreeNode<>(6);
+        TreeNode<Integer> node4 = new TreeNode<>(8);
+        TreeNode<Integer> node5 = new TreeNode<>(2);
+        TreeNode<Integer> node6 = new TreeNode<>(5);
+        TreeNode<Integer> node7 = new TreeNode<>(4);
+        TreeNode<Integer> node8 = new TreeNode<>(0);
+        TreeNode<Integer> node9 = new TreeNode<>(7);
+        root.setLeft(node1); root.setRight(node2);
+        node1.setLeft(node3); node1.setRight(node4);
+        node2.setLeft(node5); node2.setRight(node6);
+        node3.setLeft(node7);
+        node4.setLeft(node8); node4.setRight(node9);
+        return root;
+    }
+
+    public static TreeNode<Integer> getDuplicateTree()
+    {
+        TreeNode<Integer> root = new TreeNode<>(3);
+        TreeNode<Integer> node1 = new TreeNode<>(1);
+        TreeNode<Integer> node2 = new TreeNode<>(9);
+        TreeNode<Integer> node3 = new TreeNode<>(6);
+        TreeNode<Integer> node4 = new TreeNode<>(8);
+        TreeNode<Integer> node5 = new TreeNode<>(8);
+        TreeNode<Integer> node6 = new TreeNode<>(2);
+        TreeNode<Integer> node7 = new TreeNode<>(5);
+        TreeNode<Integer> node8 = new TreeNode<>(4);
+        TreeNode<Integer> node9 = new TreeNode<>(0);
+        TreeNode<Integer> node10 = new TreeNode<>(7);
+        root.setLeft(node1); root.setRight(node2);
+        node1.setLeft(node3); node1.setRight(node4);
+        node2.setLeft(node5); node2.setRight(node6);
+        node3.setLeft(node7);
+        node4.setLeft(node8); node4.setRight(node9);
+        node5.setLeft(node10);
+
+        return root;
     }
 }
